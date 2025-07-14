c1de971f579ca9286465acce7bd6bf093e5747f1
jesusnm02 <jesusricardo.nunezmateo@gmail.com>
2025-07-14 16:14:12 -0500

    camio7
    rgg

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-# from api.routes import routes # ¡Comenta esta línea temporalmente!
+from api.routes import routes
 
 app = Flask(__name__)
 CORS(app)
-# app.register_blueprint(routes) # ¡Comenta esta línea temporalmente!
-
-@app.route("/", methods=["GET"]) # Agrega una ruta simple
-def home():
-    return "¡Hola desde Flask en Azure!"
+app.register_blueprint(routes)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
