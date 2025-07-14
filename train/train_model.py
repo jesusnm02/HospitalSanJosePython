e132c37e4ee5a96d268116922ca79eace4ef9e0f
jesusnm02 <jesusricardo.nunezmateo@gmail.com>
2025-07-14 15:55:42 -0500

    cambio3
    eded

--- train/train_model.py
+++ train/train_model.py
@@ -3,15 +3,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
 
-df = pd.read_csv("train/dataset.csv")
+df = pd.read_csv("train/dataset.csv") # <--- Potential issue 1
 
 df["texto"] = df["description"]
 
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(df["texto"])
-y = df[["tratamiento", "medicamentos", "description"]] 
+y = df[["tratamiento", "medicamentos", "description"]]
 
 model = MultinomialNB()
 model.fit(X, y["medicamentos"])
-with open("app/model.pkl", "wb") as f:
+with open("app/model.pkl", "wb") as f: # <--- Potential issue 2
     pickle.dump((vectorizer, model, df), f)
